5d5bb7a1adf95e09e234512d9eeb20aad084d636
SARVESHYOGI <140194642+SARVESHYOGI@users.noreply.github.com>
2024-02-27 19:32:57 +0530

    browserrouterdom and splash screen

--- src/main.jsx
+++ src/main.jsx
@@ -3,16 +3,16 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
-import { HashRouter } from "react-router-dom"; // Import HashRouter instead of BrowserRouter
+import { BrowserRouter } from "react-router-dom"; // Import BrowserRouter instead of BrowserRouter
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <HashRouter>
+    <BrowserRouter>
       <div className="w-full">
         <App />
       </div>
-    </HashRouter>
+    </BrowserRouter>
   </React.StrictMode>
 );
 
--- src/page/SplashScreen.jsx
+++ src/page/SplashScreen.jsx
@@ -12,11 +12,11 @@ const SplashScreen = () => {
             <h1 className="text-white text-5xl md:text-8xl font-bold">
 
                 <div className="md:flex gap-7">
-                    <motion.div initial={{ opacity: 0 }} animate={{ opacity: 1 }} transition={{ delay: 0 }} className="">Hey </motion.div>
-                    <motion.div initial={{ opacity: 0 }} animate={{ opacity: 1 }} transition={{ delay: 0.3 }} className="">There ,</motion.div>
-                    <motion.div initial={{ opacity: 0 }} animate={{ opacity: 1 }} transition={{ delay: 0.6 }} className="">Explore</motion.div>
-                    <motion.div initial={{ opacity: 0 }} animate={{ opacity: 1 }} transition={{ delay: 0.9 }} className="">My </motion.div>
-                    <motion.div initial={{ opacity: 0 }} animate={{ opacity: 1 }} transition={{ delay: 1.2 }} className="">Work</motion.div>
+                    <motion.div initial={{ x: 300 }} animate={{ x: 0 }} transition={{ delay: 0 }} className="">Hey </motion.div>
+                    <motion.div initial={{ y: 300, opacity: 0 }} animate={{ y: 0, opacity: 1 }} transition={{ delay: 0.3 }} className="">There ,</motion.div>
+                    <motion.div initial={{ x: -300, opacity: 0 }} animate={{ x: 0, opacity: 1 }} transition={{ delay: 0.6 }} className="">Explore</motion.div>
+                    <motion.div initial={{ y: -300, opacity: 0 }} animate={{ y: 0, opacity: 1 }} transition={{ delay: 0.9 }} className="">My </motion.div>
+                    <motion.div initial={{ x: 300, opacity: 0 }} animate={{ x: 0, opacity: 1 }} transition={{ delay: 1.2 }} className="">Work</motion.div>
                 </div>
 
 
